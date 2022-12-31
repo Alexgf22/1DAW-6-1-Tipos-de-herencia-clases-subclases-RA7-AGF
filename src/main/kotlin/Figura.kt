709921b9998792b5a6e780eb28181b83296dea74
Alexgf22 <agarcia@iesrafaelalberti.es>
2022-12-31 19:11:21 +0100

    probando areas de figuras

--- src/main/kotlin/Figura.kt
+++ src/main/kotlin/Figura.kt
@@ -1,3 +1,7 @@
+import kotlin.math.PI
+import kotlin.math.round
+
+
 open class Figura(
     val posicionX: Int,
     val posicionY: Int
@@ -15,6 +19,15 @@ class Circulo(
     posicionY: Int
 ): Figura(posicionX, posicionY) {
 
+    constructor(radio: Int) : this(radio,0,0) {
+
+    }
+
+
+    override fun area(): String {
+        val resultado = round((PI * (radio * radio)))
+        return super.area() + resultado
+    }
 
 }
 
@@ -25,26 +38,56 @@ class Cuadrado(
     posicionY: Int
 ): Figura(posicionX, posicionY) {
 
-    //constructor(base: Int,altura: Int) : this(base,altura) {
+    constructor(base: Int,altura: Int) : this(base,altura,0,0) {
 
-    //}
+    }
+
+
 
     override fun area(): String {
-        val resultado =  2 * (base + altura)
+        val resultado =  base * altura
         return super.area() + resultado
     }
 
 }
 
-class Triangulo() {
 
 
+
+
+class Triangulo(
+    var base: Int,
+    var altura: Int,
+    posicionX: Int,
+    posicionY: Int
+): Figura(posicionX, posicionY) {
+
+    constructor(base: Int,altura: Int) : this(base,altura,0,0) {
+
+    }
+
+    override fun area(): String {
+        val resultado =  (base * altura) / 2
+        return super.area() + resultado
+    }
 
 }
 
 
+
+
 fun main() {
 
+    val cuadrado1 = Cuadrado(12,10)
 
+    val circulo1 = Circulo(20)
+
+    val triangulo1 = Triangulo(25,15)
+
+    println(cuadrado1.area())
+
+    println(circulo1.area())
+
+    println(triangulo1.area())
 
 }
